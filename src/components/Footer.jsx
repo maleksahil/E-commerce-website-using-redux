38779d5d48f69c9a5d5b68f04a0373d7cf099037
sahil <maleksahil9315@gmail.com>
2025-05-20 13:15:01 +0530

    e-shop

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,17 +30,26 @@ const Footer = () => {
           </ul>
         </div>
         <div>
-          <h4>Follow Us</h4>
-          <div>
-            <a href=''><FaFacebook/></a>
-            <a href=''><FaTwitter/></a>
-            <a href=''><FaGithub/></a>
-            <a href=''><FaLinkedin/></a>
+          <h4 className='text-lg font-semibold'>Follow Us</h4>
+          <div className='flex space-x-4 mt-4'>
+            <a href='' className='hover:text-gray-400'><FaFacebook/></a>
+            <a href='' className='hover:text-gray-400'><FaTwitter/></a>
+            <a href='' className='hover:text-gray-400'><FaGithub/></a>
+            <a href='' className='hover:text-gray-400'><FaLinkedin/></a>
           </div>
-          <form>
-            <input type='text'/>
-            <button>Subscribe</button>
+          <form className='flex items-center justify-center mt-8'>
+            <input type='email' placeholder='Enter Email' className='w-full p-2 rounded-l-lg bg-gray-800 border border-gray-600'/>
+            <button className='bg-red-600 text-white px-4 py-2 rounded-r-lg border border-gray-600'>Subscribe</button>
           </form>
+        </div>
+      </div>
+      <div className='mt-8 border-t border-gray-700 pt-4'>
+        <div className='container mx-auto flex flex-col md:flex-row justify-between items-center'>
+          <p>&copy; 2024 e-shop All rights reserved</p>
+          <div className='flex space-x-4 mt-4 md:mt-0'>
+            <a href=''>Privacy Policy</a>
+            <a href=''>Terms & Condition</a>
+          </div>
         </div>
       </div>
     </footer>
